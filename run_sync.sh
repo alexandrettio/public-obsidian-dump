eafe0bc6e30d07539aaad6812fdad4b7002f2f04
Aleksandra Vorontsova <alexandrettio@gmail.com>
2025-06-14 10:14:02 +0300

    Quartz sync: Jun 14, 2025, 10:14 AM

--- run_sync.sh
+++ run_sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export PATH=/usr/local/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/quartz_launchd
 
 cd /Users/alexandret/my-quartz-obsidian-publisher
 /usr/local/bin/npx quartz sync --no-pull
